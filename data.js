9ea626ebbe3c26791fd444a0e5ca06afc4c0f713
joabeleonard <joabe.leonard.feitosa@gmail.com>
2017-12-11 00:08:59 -0300

    Commit Inicial

---
+++ data.js
@@ -0,0 +1,39 @@
+const jsonfile = require('jsonfile-promised');
+const fs = require('fs');
+
+
+module.exports = {
+ salvaDados(curso, tempoEstudado){
+        let arquivoDoCurso = __dirname + '/data/' + curso + '.json';
+        if(fs.existsSync(arquivoDoCurso)){
+            this.adicionaTempoAoCurso(arquivoDoCurso, tempoEstudado);
+        }else{
+            // Criar Arquivo 
+            this.criaArquivoDeCurso(arquivoDoCurso, {})
+                .then(()=>{
+                    this.adicionaTempoAoCurso(arquivoDoCurso, tempoEstudado);
+                })
+        }
+    },
+    adicionaTempoAoCurso(arquivoDoCurso, tempoEstudado){
+        let dados = {
+            ultimoEstudo : new Date().toString(),
+            tempo: tempoEstudado
+        }
+
+        jsonfile.writeFile(arquivoDoCurso, dados, {spaces: 2})
+            .then(()=>{
+                console.log('Tempo salvo com sucesso')
+            }).catch((err) => {
+                console.log(err);
+            })
+    },
+criaArquivoDeCurso(nomeArquivo,conteudoArquivo){
+        return jsonfile.writeFile(nomeArquivo, conteudoArquivo)
+            .then(()=>{
+                console.log('Arquivo Criado');
+            }).catch((err) => {
+                console.log(err);
+            })
+    }
+}
---
+++ main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow, ipcMain}  = require('electron');
+const data = require('./data');
+
+app.on('ready', () => {
+    console.log('Aplicacão iniciada');
+    let mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 768,
+    });
+
+    mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+
+});
+
+app.on('window-all-closed', () => {
+    app.quit();
+});
+
+let sobreWindow = null;
+ipcMain.on('abrir-janela-sobre', () => {
+    if(sobreWindow == null){
+        sobreWindow = new BrowserWindow({
+            width: 300,
+            height: 220,
+            alwaysOnTop: true,
+            frame: false
+        });
+        sobreWindow.on('closed', () => {
+            sobreWindow = null;
+        })
+    }
+    sobreWindow.loadURL(`file://${__dirname}/app/sobre.html`);
+
+});
+
+ipcMain.on('fechar-janela-sobre', () => {
+    sobreWindow.close();
+});
+
+ipcMain.on('curso-parado', (event, curso, tempoEstudado) => {
+    console.log(`O curso ${curso} foi estudado por ${tempoEstudado}`);
+    data.salvaDados(curso,tempoEstudado);
+});
