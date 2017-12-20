5ba3788b3f09e44233e808c30d65d88c39545df2
Joabe Leonard Feitosa <joabe.leonard@seplag.local>
2017-12-20 14:28:23 -0300

    Pagina para cadastrar

--- app/table.js
+++ app/table.js
@@ -84,7 +84,7 @@ window.$(document).ready(function() {
     window.$('#new').click( function (e) {
     e.preventDefault();
  
-    var aiNew = oTable.fnAddData( [ '', '', '', '', '',
+    var aiNew = oTable.add( [ '', '', '', '', '','','','',
         '[Edit]()', '[Delete]()' ] );
     var nRow = oTable.fnGetNodes( aiNew[0] );
     editRow( oTable, nRow );
@@ -94,7 +94,7 @@ window.$(document).ready(function() {
     window.$('#dataTable').on('click', 'a.delete',function (e) {
         e.preventDefault();
     
-        var nRow = $(this).parents('tr')[0];
+        var nRow = window.$(this).parents('tr')[0];
         oTable.fnDeleteRow( nRow );
     } );
 } );
