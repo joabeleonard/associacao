efd7c4e25a0ed890f924acd6f87cf4244bcdac74
joabeleonard <joabe.leonard.feitosa@gmail.com>
2017-12-21 23:17:22 -0300

    Adicionar associado

---
+++ app/js/renderer.js
@@ -0,0 +1,24 @@
+let botaoSalvar = document.querySelector('#btnSalvarAssosiado');
+
+let inputName = document.querySelector('#exampleInputName');
+let inputName = document.querySelector('#exampleInputName');
+let inputName = document.querySelector('#exampleInputName');
+let inputName = document.querySelector('#exampleInputName');
+let inputName = document.querySelector('#exampleInputName');
+let inputName = document.querySelector('#exampleInputName');
+let inputName = document.querySelector('#exampleInputName');
+let inputName = document.querySelector('#exampleInputName');
+
+
+
+botaoSalvar.addEventListener('click', function () {
+    let nome = inputName.textContent;
+    let nome = inputName.textContent;
+    let nome = inputName.textContent;
+    let nome = inputName.textContent;
+    let nome = inputName.textContent;
+    let nome = inputName.textContent;
+    let nome = inputName.textContent;
+    let nome = inputName.textContent;
+
+});
