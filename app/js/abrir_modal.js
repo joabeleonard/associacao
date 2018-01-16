e1e9e2f60b6e01c34922b186c75c20bc3acd424b
joabeleonard <joabe.leonard.feitosa@gmail.com>
2018-01-15 23:58:07 -0300

    Ajustes

--- app/js/abrir_modal.js
+++ app/js/abrir_modal.js
@@ -1,22 +1,11 @@
 
 window.$(document).ready(function(){
-    window.$("#myBtn").click(function(){
-        window.$("#myModal").modal();
-     inputName = "";
-     inputNomeDeGuerra = "";
-     inputEmail = "";
-     inputCPF = "";
-     inputPosto = "";
-     inputOrgao = "";
-     inputTurma = "";
-     inputDataNascimento = "";
-     inputHiddenId = "";
-    });
+   
 
      window.$('#myModal').on('hidden.bs.modal', function() {
-    console.log('fechar modal')
-    $(this).find('input:text').val('');
-    $(this).find('input').val('');
-    
-  });
+        console.log('fechar modal')
+        $(this).find('input:text').val('');
+        $(this).find('input').val('');
+        
+     });
 });
